369258c6eb3f6699f7d047a10c35c375c2e29783
Pranav-Badrinathan <40524763+Pranav-Badrinathan@users.noreply.github.com>
2019-02-15 00:16:36 -0500

    Added basic player movement
    added quad to act as player for time being

---
+++ Mock Game Jam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float walkSpeed;
+	public float sprintSpeed;
+
+	private float speed;
+
+	private void Update()
+	{
+		//Set Speed
+		if (Input.GetKey(KeyCode.LeftShift))
+		{
+			speed = sprintSpeed;
+		}
+		else
+		{
+			speed = walkSpeed;
+		}
+
+		//Get Player Input
+		float xMove = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+		float yMove = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
+
+		//Apply Input
+		transform.position += new Vector3(xMove, yMove);
+	}
+}
