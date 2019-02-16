d33851777a82673a0f832aacbfa38e0aebb67577
Pranav-Badrinathan <40524763+Pranav-Badrinathan@users.noreply.github.com>
2019-02-16 17:50:33 -0500

    Reflection Fixed!!
    finally did it!

--- Mock Game Jam/Assets/Scripts/MirrorReflect.cs
+++ Mock Game Jam/Assets/Scripts/MirrorReflect.cs
@@ -1,44 +1,56 @@
 ﻿using UnityEngine;
-
 [RequireComponent(typeof(LineRenderer))]
 public class MirrorReflect : MonoBehaviour
 {
-	public LayerMask layer;
-
+	//the attached line renderer
 	private LineRenderer lineRenderer;
 
-	private void Start()
+	//the number of reflections
+	public int nReflections = 2;
+	//max length
+	public float maxLength = 100f;
+
+	//private int pointCount;
+	void Awake()
 	{
+		//get the attached LineRenderer component  
 		lineRenderer = GetComponent<LineRenderer>();
 	}
-
-	private void Update()
+	void Update()
 	{
-		CalculateReflections(transform.position + new Vector3(1, 0), transform.right);
-	}
+		//clamp the number of reflections between 1 and int capacity  
+		nReflections = Mathf.Clamp(nReflections, 1, nReflections);
 
-	private void CalculateReflections(Vector3 position, Vector3 direction)
-	{
-		int reflectionIndex = 1;
+		Ray2D ray = new Ray2D(transform.position, transform.right);
 
-		RaycastHit2D hit = Physics2D.Raycast(position, direction, 100, layer);
-		if (hit.collider != null)
+		//start with just the origin
+		lineRenderer.positionCount = 1;
+		lineRenderer.SetPosition(0, transform.position);
+		float remainingLength = maxLength;
+		//bounce up to n times
+		for (int i = 0; i < nReflections; i++)
 		{
-			reflectionIndex++;
-
-			lineRenderer.positionCount = reflectionIndex;
-			lineRenderer.SetPosition(reflectionIndex - 1, hit.point);
-
-			Debug.Log(hit.collider.gameObject.name);
-
-			if (hit.transform.gameObject.layer == layer)
+			RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, remainingLength);
+			// ray cast
+			if (hit.collider != null)
 			{
-				CalculateReflections(hit.point, hit.normal);
+				//we hit, update line renderer
+				lineRenderer.positionCount += 1;
+				lineRenderer.SetPosition(lineRenderer.positionCount - 1, hit.point);
+				// update remaining length and set up ray for next loop
+				remainingLength -= Vector3.Distance(ray.origin, hit.point);
+				ray = new Ray2D(hit.point, Vector3.Reflect(ray.direction, hit.normal));
+				// break loop if we don't hit a Mirror
+				if (hit.collider.tag != "Mirror")
+					break;
 			}
-		}
-		else
-		{
-			return;
+			else
+			{
+				// We didn't hit anything, draw line to end of ramainingLength
+				lineRenderer.positionCount += 1;
+				lineRenderer.SetPosition(lineRenderer.positionCount - 1, ray.origin + ray.direction * remainingLength);
+				break;
+			}
 		}
 	}
 }
